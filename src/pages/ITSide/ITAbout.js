047051e908cb734a70223866eb96baecaf4c7868
ACaffeinatedCoder <97655140+ACaffeinatedCoder@users.noreply.github.com>
2023-03-24 17:09:54 +0800

    Add files via upload

---
+++ src/pages/ITSide/ITAbout.js
@@ -0,0 +1,10 @@
+import INavigationBar from "../../ITNavBar";
+
+export default function IAbout() {
+    return (
+        <div>
+            <INavigationBar/>
+            <h1>About</h1>
+        </div>
+    ) 
+}
